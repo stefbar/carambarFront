655cf6dcfa3152e61e61b57135f5ed81a4b91e32
stefbar <stef.barucq@gmail.com>
2024-05-20 18:01:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function getRandomJoke() {
+    fetch("http://localhost:3000/api.carambarJokes/v1.0.0/random-joke/:randomId")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("getJokeId").innerHTML = data.id;
+        document.getElementById("getJoke").innerHTML = data.joke;
+        document.getElementById("getJokeAnswer").innerHTML = data.answer;
+        document.getElementById("getJokeCategory").innerHTML = data.category_id;
+    })
+}
+document.getElementById("getRandomJoke-btn").addEventListener('click',
+() => getRandomJoke())
+
+function getAllJokes() {
+    fetch("http://localhost:3000/api.carambarJokes/v1.0.0/jokes")
+    .then(response => response.json())
+    .then(data => {
+        console.log('data: ', data);
+    })
+}
+document.getElementById("getAllJokes-btn").addEventListener('click', getAllJokes)
