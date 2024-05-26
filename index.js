c8a7a3c1655ea1e8ef3c175f8eda9910aba5b27f
stefbar <stef.barucq@gmail.com>
2024-05-26 21:58:13 +0200

    start to add on scroll event for header logo

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+const logoCarambar = document.getElementById("header-carambar-logo");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add(scrollDown);
+    // logoCarambar.style.opacity = 0;
+  } else {
+    document.querySelector(".header").classList.remove(scrollDown);
+    // logoCarambar.style.opacity = 1;
+  }
+});
+
 function getRandomJoke() {
     fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
     .then(response => response.json())
     .then(data => {
         // document.getElementById("getJokeId").innerHTML = data.id;
-        document.getElementById("getJoke").innerHTML = data.joke;
-        document.getElementById("getJokeAnswer").innerHTML = data.answer;
+        document.getElementById("getJoke").innerHTML = data.joke
+        document.getElementById("getJokeAnswer").innerHTML = data.answer
         // document.getElementById("getJokeCategory").innerHTML = data.category_id;
     })
 }
