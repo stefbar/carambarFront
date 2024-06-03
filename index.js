fbeff9067b950090b3ee130228b738c0431196d3
stefbar <stef.barucq@gmail.com>
2024-06-03 17:12:54 +0200

    simplifying style achieved, & add throttle to cta

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const header = document.querySelector("header");
+const header = document.querySelector("header")
 window.addEventListener("scroll", (event) => {
   if (document.body.scrollTop > 10) {
-    header.style.top = "-100px";
+    header.style.top = "-100px"
   } else {
-    header.style.top = "0";
+    header.style.top = "0"
   }
-}, true);
+}, true)
 
-function getRandomJoke() {
+// throttle to prevent case when cta is clicked multiple times: answer appears 4 seconds after first click so it remains with the good joke
+function throttle(mainFunction, delay) {
+  let timerFlag = null
+  return (...args) => {
+    if (timerFlag === null) {
+      mainFunction(...args)
+      timerFlag = setTimeout(() => {
+        timerFlag = null
+      }, delay)
+    }
+  }
+}
+  function fetchRandomJoke() {
     fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
     .then(response => response.json())
     .then(data => {
-        // document.getElementById("getJokeId").innerHTML = data.id;
-        document.getElementById("getJoke").innerHTML = data.joke
-        document.getElementById("getJokeAnswer").innerHTML = ""
-        setTimeout(() => {
-          document.getElementById("getJokeAnswer").innerHTML = data.answer
-        }, 4000)
-        // document.getElementById("getJokeCategory").innerHTML = data.category_id;
+      const jokeCategory = data.category_id;
+      document.getElementById("getJoke").innerHTML = data.joke
+      document.getElementById("getJokeAnswer").innerHTML = ""
+      jokeCategory === 2 ? setTimeout(() => {
+        document.getElementById("getJokeAnswer").innerHTML = data.answer
+      }, 4000) : null
     })
-}
-document.getElementById("getRandomJoke-btn").addEventListener('click',
-() => getRandomJoke())
+  }
+  const throttledFetch = throttle(fetchRandomJoke, 4000)
+
+  document.getElementById("getRandomJoke-btn").addEventListener('click', throttledFetch)
 
 // function getAllJokes() {
 //     fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/jokes")
