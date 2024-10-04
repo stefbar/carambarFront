60fb7811b5e1172b8c0c377317d80606579d96a4
stefbar <stef.barucq@gmail.com>
2024-10-04 11:36:14 +0200

    added loading anim when fetching random joke

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ window.addEventListener("scroll", (event) => {
   }
 }, true)
 
+// fetching response loader
+const loader = document.getElementById('loader')
+// const jokeElement = document.getElementById('joke')
+document.getElementById('loader').style.display = 'none'
+
 // throttle to prevent case when cta is clicked multiple times: answer appears 4 seconds after first click so it remains with the good joke
 function throttle(mainFunction, delay) {
   let timerFlag = null
@@ -19,18 +24,39 @@ function throttle(mainFunction, delay) {
     }
   }
 }
-  function fetchRandomJoke() {
-    fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
-    .then(response => response.json())
-    .then(data => {
-      const jokeCategory = data.category_id;
-      document.getElementById("getJoke").innerHTML = data.joke
-      document.getElementById("getJokeAnswer").innerHTML = ""
-      jokeCategory === 2 ? setTimeout(() => {
-        document.getElementById("getJokeAnswer").innerHTML = data.answer
-      }, 4000) : null
-    })
+  async function fetchRandomJoke() {
+      loader.style.display = 'flex'
+      try {
+          const response = await fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
+          const data = await response.json()
+          const jokeCategory = data.category_id
+          document.getElementById("getJoke").innerHTML = data.joke
+          document.getElementById("getJokeAnswer").innerHTML = ""
+          jokeCategory === 2 ? setTimeout(() => {
+              document.getElementById("getJokeAnswer").innerHTML = data.answer
+          }, 4000) : null
+      } catch (error) {
+          console.log(error)
+      } finally {
+          loader.style.display = 'none'
+      }
   }
+
+    // fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
+    // .then(response => response.json())
+    // .then(data => {
+    //   const jokeCategory = data.category_id;
+    //   document.getElementById("getJoke").innerHTML = data.joke
+    //   document.getElementById("getJokeAnswer").innerHTML = ""
+    //   jokeCategory === 2 ? setTimeout(() => {
+    //     document.getElementById("getJokeAnswer").innerHTML = data.answer
+    //   }, 4000) : null
+    // })
+    // .catch(error => console.log(error))
+    // .finally(() => {
+    //   loader.style.display = 'none'
+    // })
+  // }
   const throttledFetch = throttle(fetchRandomJoke, 4000)
 
   document.getElementById("getRandomJoke-btn").addEventListener('click', throttledFetch)
