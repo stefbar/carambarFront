7b2f742d14e86ca670eedeb22221d411c1911ab3
stef bar <stef.barucq@gmail.com>
2024-10-29 19:03:43 +0100

    clened files

--- index.js
+++ index.js
@@ -26,62 +26,40 @@ function throttle(mainFunction, delay) {
 }
   async function fetchRandomJoke() {
       loader.style.display = 'flex'
-  //     try {
-  //         const response = await fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
-  //         const data = await response.json()
-  //         const jokeCategory = data.category_id
-  //         document.getElementById("getJoke").innerHTML = data.joke
-  //         document.getElementById("getJokeAnswer").innerHTML = ""
-  //         jokeCategory === 2 ? setTimeout(() => {
-  //             document.getElementById("getJokeAnswer").innerHTML = data.answer
-  //         }, 4000) : null
-  //     } catch (error) {
-  //         console.log(error)
-  //     } finally{() => {
-  //         loader.style.display = 'none'
-  //     }}
-  // }
-
-    fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
-    .then(response => response.json())
-    .then(data => {
-      const jokeCategory = data.category_id;
-      document.getElementById("getJoke").innerHTML = data.joke
-      document.getElementById("getJokeAnswer").innerHTML = ""
-      jokeCategory === 2 ? setTimeout(() => {
-        document.getElementById("getJokeAnswer").innerHTML = data.answer
-      }, 4000) : null
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      loader.style.display = 'none'
-    })
+      try {
+          const response = await fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
+          const data = await response.json()
+          const jokeCategory = data.category_id
+          document.getElementById("getJoke").innerHTML = data.joke
+          document.getElementById("getJokeAnswer").innerHTML = ""
+          jokeCategory === 2 ? setTimeout(() => {
+              document.getElementById("getJokeAnswer").innerHTML = data.answer
+          }, 4000) : null
+      } catch (error) {
+          console.log(error)
+      } finally{() => {
+          loader.style.display = 'none'
+      }}
   }
+
+  //   fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     const jokeCategory = data.category_id;
+  //     document.getElementById("getJoke").innerHTML = data.joke
+  //     document.getElementById("getJokeAnswer").innerHTML = ""
+  //     jokeCategory === 2 ? setTimeout(() => {
+  //       document.getElementById("getJokeAnswer").innerHTML = data.answer
+  //     }, 4000) : null
+  //   })
+  //   .catch(error => console.log(error))
+  //   .finally(() => {
+  //     loader.style.display = 'none'
+  //   })
+  // }
   const throttledFetch = throttle(fetchRandomJoke, 4000)
 
   document.getElementById("getRandomJoke-btn").addEventListener('click', throttledFetch)
-
-// function getRandomJoke() {
-//     fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
-//     .then(response => response.json())
-//     .then(data => {
-//         // document.getElementById("getJokeId").innerHTML = data.id;
-//         document.getElementById("getJoke").innerHTML = data.joke;
-//         document.getElementById("getJokeAnswer").innerHTML = data.answer;
-//         // document.getElementById("getJokeCategory").innerHTML = data.category_id;
-//     })
-// }
-// document.getElementById("getRandomJoke-btn").addEventListener('click',
-// () => getRandomJoke())
-
-// function getAllJokes() {
-//     fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/jokes")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('data: ', data);
-//     })
-// }
-// document.getElementById("getAllJokes-btn").addEventListener('click', getAllJokes)
 
 // function getAllJokes() {
 //     fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/jokes")
