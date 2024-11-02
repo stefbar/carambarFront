50abd24f6799ff74f6907094b588e26397a3b094
stef bar <stef.barucq@gmail.com>
2024-11-02 10:44:09 +0100

    issue with loader persisting

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ window.addEventListener("scroll", (event) => {
 
 // fetching response loader
 const loader = document.getElementById('loader')
-// const jokeElement = document.getElementById('joke')
-document.getElementById('loader').style.display = 'none'
+loader.style.display = 'none'
 
 // throttle to prevent case when cta is clicked multiple times: answer appears 4 seconds after first click so it remains with the good joke
 function throttle(mainFunction, delay) {
@@ -26,37 +25,38 @@ function throttle(mainFunction, delay) {
 }
   async function fetchRandomJoke() {
       loader.style.display = 'flex'
-  //     try {
-  //         const response = await fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
-  //         const data = await response.json()
-  //         const jokeCategory = data.category_id
-  //         document.getElementById("getJoke").innerHTML = data.joke
-  //         document.getElementById("getJokeAnswer").innerHTML = ""
-  //         jokeCategory === 2 ? setTimeout(() => {
-  //             document.getElementById("getJokeAnswer").innerHTML = data.answer
-  //         }, 4000) : null
-  //     } catch (error) {
-  //         console.log(error)
-  //     } finally{() => {
-  //         loader.style.display = 'none'
-  //     }}
-  // }
-
-    fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
-    .then(response => response.json())
-    .then(data => {
-      const jokeCategory = data.category_id;
-      document.getElementById("getJoke").innerHTML = data.joke
-      document.getElementById("getJokeAnswer").innerHTML = ""
-      jokeCategory === 2 ? setTimeout(() => {
-        document.getElementById("getJokeAnswer").innerHTML = data.answer
-      }, 4000) : null
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      loader.style.display = 'none'
-    })
+      try {
+          const response = await fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
+          const data = await response.json()
+          console.log('data: ', data)
+          const jokeCategory = data.category_id
+          document.getElementById("getJoke").innerHTML = data.joke
+          document.getElementById("getJokeAnswer").innerHTML = ""
+          jokeCategory === 2 ? setTimeout(() => {
+              document.getElementById("getJokeAnswer").innerHTML = data.answer
+          }, 4000) : null
+      } catch (error) {
+          console.log(error)
+      } finally{() => {
+          loader.style.display = 'none'
+      }}
   }
+
+  //   fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
+  //   .then(response => response.json())
+  //   .then(data => {  
+  //     const jokeCategory = data.category_id;
+  //     document.getElementById("getJoke").innerHTML = data.joke
+  //     document.getElementById("getJokeAnswer").innerHTML = ""
+  //     jokeCategory === 2 ? setTimeout(() => {
+  //       document.getElementById("getJokeAnswer").innerHTML = data.answer
+  //     }, 4000) : null
+  //   })
+  //   .catch(error => console.log(error))
+  //   .finally(() => {
+  //     loader.style.display = 'none'
+  //   })
+  // }
   const throttledFetch = throttle(fetchRandomJoke, 4000)
 
   document.getElementById("getRandomJoke-btn").addEventListener('click', throttledFetch)
