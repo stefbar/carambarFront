9224355c1728b0a0ec627ebf3b6e3c91e70206b8
stef bar <stef.barucq@gmail.com>
2024-11-04 17:11:52 +0100

    added progress bar for between joke and its answer to be displayed

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ window.addEventListener("scroll", (event) => {
 const loader = document.getElementById('loader')
 loader.style.display = 'none'
 
+// progress bar
+const progressBar = document.getElementById('jokeAnswerComingProgressBar')
+progressBar.style.display = 'none'
+let intervalId
+
+let currentProgress = 0
+const maxProgress = 400
+
+function updateProgress() {
+  // progressBar.style.display = 'flex'
+  currentProgress++
+  progressBar.style.width = `${(currentProgress / maxProgress) * 100}%`
+  if (currentProgress >= maxProgress) {
+    clearInterval(intervalId)
+    progressBar.style.display = 'none'
+  }
+}
+
 // throttle to prevent case when cta is clicked multiple times: answer appears 4 seconds after first click so it remains with the good joke
 function throttle(mainFunction, delay) {
   let timerFlag = null
@@ -25,19 +43,30 @@ function throttle(mainFunction, delay) {
 }
   async function fetchRandomJoke() {
       loader.style.display = 'flex'
+      progressBar.style.display = 'flex'
+      progressBar.style.width = '0%'
+      currentProgress = 0
+
+      if(intervalId) {
+          clearInterval(intervalId)
+      }
+      intervalId = setInterval(updateProgress, 10)
+
       try {
           const response = await fetch("https://api-carambarjokes.onrender.com/api.carambarJokes/v1.0.0/random-joke/:randomId")
           const data = await response.json()
           console.log('data: ', data)
           const jokeCategory = data.category_id
           document.getElementById("getJoke").innerHTML = data.joke
+          
+          
           document.getElementById("getJokeAnswer").innerHTML = ""
           jokeCategory === 2 ? setTimeout(() => {
               document.getElementById("getJokeAnswer").innerHTML = data.answer
           }, 4000) : null
       } catch (error) {
           console.log(error)
-      } finally{
+      } finally {
           loader.style.display = 'none'
       }
   }
